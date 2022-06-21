fdbdd07b277a238b1d0b07cb86b53580528fbf28
Aleyna Elif ÖZKAN <aleynaelifozkan@hotmail.com>
2022-06-21 23:22:32 +0300

    musictify initial

---
+++ MusictifyApp/app/src/main/java/com/ley/musictifyapp/HomePage.kt
@@ -0,0 +1,63 @@
+package com.ley.musictifyapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.ley.musictifyapp.databinding.ActivityHomePageBinding
+
+class HomePage : AppCompatActivity() {
+    private lateinit var binding: ActivityHomePageBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        replaceFragment(Home())
+
+        auth = Firebase.auth
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.search -> replaceFragment(Search())
+
+                else->{
+
+                }
+            }
+            true
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.sign_out,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.signout){
+            auth.signOut()
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+    }
+}
